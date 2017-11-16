dc72f8a2cf65acccdc622051d23e51f0eda1c1e7
Peter Jang <peter.x.jang@gmail.com>
2017-11-15 19:43:14 -0600

    Add solution to name message exercise

--- app/controllers/v1/params_examples_controller.rb
+++ app/controllers/v1/params_examples_controller.rb
@@ -7,4 +7,13 @@ class V1::ParamsExamplesController < ApplicationController
       carrots_message: "There are currently #{num_carrots} of carrots."
     }
   end
+
+  def name_method
+    input_name = params["query_name"]
+    special_message = "Your name sucks."
+    if input_name.upcase.start_with?("A")
+      special_message = "Hey your name starts with A!"
+    end
+    render json: {message: "Hey your name is #{input_name}", message2: special_message}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :v1 do
     get "/query_params_url" => "params_examples#query_params_method"
+
+    get "/name_url" => "params_examples#name_method"
   end
 end
